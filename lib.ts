ff0cac01346c2578d520666f1d2b6c9e5de22ff3
Sol Boucher <sboucher@cmu.edu>
2021-01-22 17:07:05 -0500

    lib.ts: Add synchronous prompt()/window.prompt() polyfill

--- lib.ts
+++ lib.ts
@@ -1,14 +1,28 @@
 if(!globalThis.window) {
+	let getline: () => string | null = require('./rary');
+
 	let globalThat = Object.getPrototypeOf(globalThis);
 	globalThat.alert = function(message: {} = '') {
 		console.log(message);
+	};
+	globalThat.prompt = function(message = 'Enter text:', delimiter = ' ') {
+		process.stdout.write(message + delimiter);
+
+		let line = getline();
+		if(line)
+			line = line.trim();
+		else
+			console.log();
+		return line;
 	};
 	globalThis.window = globalThat;
 }
 
 export let window: {
 	alert(_?: {}): void,
+	prompt(_?: string, delimiter?: string): string | null,
 } = {
 	alert: globalThis.alert.bind(globalThis),
+	prompt: globalThis.prompt.bind(globalThis),
 };
-export let {alert} = window;
+export let {alert, prompt} = window;
